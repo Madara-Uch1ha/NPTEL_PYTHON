397a6b7d8a54ad3147e9243f77377a6123693eef
Madara-Uch1ha <nitpow007@gmail.com>
2023-02-11 13:19:22 +0530

    Need help!!

--- week3.py
+++ week3.py
@@ -8,5 +8,30 @@
 
 >>> remdup([3,5,7,5,3,7,10])
 [5, 3, 7, 10]'''
+def B_s(array,key):
+    low=0
+    high=len(array)
+    for i in range(len(array)):
+        mid=int((low+high)/2)
+        if key==mid:
+            return(mid)
+        elif(key<mid):
+            high=mid-1
+        else:
+            low=mid+1
+    return(-1)
 def remdup(l):
-    return(l)
+    i=0
+    for i in range(len(l)):
+        dup=l[i:len(l)]
+        key=l[i]
+        a=B_s(dup,key)
+        if a!=0:
+            l.pop(a)      
+    return(dup)
+'''a=[1,2,3,4,5]
+i=2
+c=a[i:len(a)]
+print(c)'''
+print(remdup([3,1,3,5]))
+# a=B_s([3,1,3,5],9)
