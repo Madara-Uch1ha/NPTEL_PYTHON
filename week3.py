a50639d6e459fb43690586512d21c2d7256b4cdd
Madara-Uch1ha <nitpow007@gmail.com>
2023-02-11 12:24:51 +0530

    Created week 3 file yet to solve

---
+++ week3.py
@@ -0,0 +1,10 @@
+'''Define a Python function remdup(l) that takes a nonempty list of integers l and removes all duplicates in l, keeping only the last occurrence of each number. For instance:
+
+>>> remdup([3,1,3,5])
+[1, 3, 5]
+
+>>> remdup([7,3,-1,-5])
+[7, 3, -1, -5]
+
+>>> remdup([3,5,7,5,3,7,10])
+[5, 3, 7, 10]'''
