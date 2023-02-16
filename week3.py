a940d7c056288e2f5fdd311f77782743427bbbaf
Madara-Uch1ha <nitpow007@gmail.com>
2023-02-16 16:55:51 +0530

    reviwing...final touches

--- week3.py
+++ week3.py
@@ -8,37 +8,33 @@
 
 >>> remdup([3,5,7,5,3,7,10])
 [5, 3, 7, 10]'''
-def B_s(array,key):
+def binary_search(array, key):
     low=0
-    high=len(array)
-    for i in range(len(array)):
-        mid=int((low+high)/2)
-        if key==mid:
-            return(mid)
-        elif(key<mid):
-            high=mid-1
-        else:
+    high=len(array)-1
+    while low<=high:
+        mid=(low+high)//2
+        if array[mid]==key:
+            return mid
+        elif array[mid]<key:
             low=mid+1
-    return(-1)
+        else:
+            high=mid-1
+    return -1
 def remdup(l):
     i=0
-    for i in range(len(l)):
-        dup=l[i:len(l)]
+    while i<len(l):
         key=l[i]
-        a=B_s(dup,key)
-        if a!=0:
-            l.pop(a)      
-    return(dup)
+        a=binary_search(l[i+1:],key)
+        if a!=-1:           
+            l.pop(a+i+1)
+        else:
+            i+=1
+    return l
+
 '''a=[1,2,3,4,5]
 i=2
 c=a[i:len(a)]
 print(c)'''
 print(remdup([3,1,3,5]))
-#ERROR
-'''Traceback (most recent call last):
-  File "z:\Python\GitRep\NPTEL_PYTHON\week3.py", line 36, in <module>
-    print(remdup([3,1,3,5]))
-  File "z:\Python\GitRep\NPTEL_PYTHON\week3.py", line 27, in remdup
-    key=l[i]
-IndexError: list index out of range'''
+
 # a=B_s([3,1,3,5],9)
